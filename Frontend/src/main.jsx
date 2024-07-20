b877118dea78e00d88b5adb8218a8b7819afe3c3
Mr-Bhagatwala <154253285+Mr-Bhagatwala@users.noreply.github.com>
2024-07-20 19:21:44 +0530

    test

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -8,7 +8,6 @@ import StoreContextProvider from './context/StoreContext.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
   <StoreContextProvider>
-
     <App />
   </StoreContextProvider>
   </BrowserRouter>
